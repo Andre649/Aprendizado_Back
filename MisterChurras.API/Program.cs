8c80b627da2613499ad5354b816a9c143da760b3
André dos Santos <111886226+Andre649@users.noreply.github.com>
2025-11-17 20:07:59 +0000

    Removendo redirecionamento HTTPS para corrigir erro no Render

--- MisterChurras.API/Program.cs
+++ MisterChurras.API/Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // --- 2. Configuração do Pipeline ---
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 // Avisa ao app: "Mapeie os controllers que encontrar e crie as rotas"
 app.MapControllers();
